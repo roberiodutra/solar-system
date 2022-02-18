0779238e3123d054eaa0ce946522e6b6e1440875
Robério Almeida <dev.roberio@gmail.com>
2022-02-18 18:00:15 -0300

    feat: cria um componente chamado PlanetCard

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropType.string,
+  planetImage: PropType.string,
+};
+
+PlanetCard.defaultProps = {
+  planetName: 'name',
+  planetImage: 'image',
+};
+
+export default PlanetCard;
