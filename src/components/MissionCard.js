35b055f8f0eef2b787bb5d1dcbdccff198bf9a3c
Robério Almeida <dev.roberio@gmail.com>
2022-02-18 20:07:35 -0300

    feat: cria um componente chamado MissionCard

---
+++ src/components/MissionCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { destination, country, year, name } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  destination: PropType.string,
+  country: PropType.string,
+  year: PropType.string,
+  name: PropType.string,
+};
+
+MissionCard.defaultProps = {
+  destination: 'destination.isRequired',
+  country: 'country.isRequired',
+  year: 'year.isRequired',
+  name: 'name.isRequired',
+};
+
+export default MissionCard;
